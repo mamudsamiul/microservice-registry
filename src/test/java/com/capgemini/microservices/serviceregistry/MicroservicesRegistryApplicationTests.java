6db79e271eeba42b50e467255ba6cc7404c4d455
mamudsamiul <samiulmamud1995@gmail.com>
2021-03-03 10:46:40 +0530

    (Samiul Mamud) Created: Microservice Registry is created

---
+++ src/test/java/com/capgemini/microservices/serviceregistry/MicroservicesRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.microservices.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
